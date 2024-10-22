004a70ffa232c0cad294d61b7744e835885128c8
teaqu <teaqu@outlook.com>
2024-10-22 22:07:54 +0100

    spiral

--- spiral/sketch.js
+++ spiral/sketch.js
@@ -1,19 +1,21 @@
 var angle = 0.0;
-var offset = 60;
+var offset = 220;
 var scalar = 2;
-var speed = 0.1;
+var speed = 0.01;
 
 function setup() {
-  createCanvas(120, 120);
+  createCanvas(440, 440);
   fill(0);
   background(204);
-	frameRate(600);
+  frameRate(144);
 }
 
 function draw() {
-  var x = offset + cos(angle) * scalar;
-  var y = offset + sin(angle) * scalar;
-  ellipse(x, y, 2, 2);
-  angle += speed;
-  scalar += speed;
+  for (var i = 0; i < 100; ++i) {
+    var x = offset + cos(angle) * scalar;
+    var y = offset + sin(angle) * scalar;
+    ellipse(x, y, 2, 2);
+    angle += speed;
+    scalar += speed;
+  }
 }
