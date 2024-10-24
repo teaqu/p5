8cc9f1872fa814f71a67b17ba46a1ef57d077b31
teaqu <teaqu@outlook.com>
2024-10-24 22:54:55 +0100

    noc

--- noc/ch1/sketch.js
+++ noc/ch1/sketch.js
@@ -1,28 +1,44 @@
 class Walker {
 	constructor() {
-		this.x = width/2;
-		this.y = height/2;
+		this.x = random(width);
+		this.y = random(height);
+    this.color = [floor(random(255)), floor(random(255)), floor(random(255))];
 	}
 	
 	show() {
-		stroke(1);
+		stroke(this.color[0], this.color[1], this.color[2]);
+    strokeWeight(5);
 	  point(this.x, this.y);
 	}
 	
 	step() {
-    let xstep = floor(random(3)) - 1;
-    let ystep = floor(random(3)) - 1;
-    this.x += xstep;
-    this.y += ystep;
+    let xstep = random(-3, 3);
+    let ystep = random(-3, 3);
+
+    const col = get(this.x + xstep, this.y + ystep);
+    if ((col[0] == 255 && col[1] == 255 && col[2] == 255) || 
+      (col[0] == this.color[0] && col[1] == this.color[1] && col[2] == this.color[2])) {
+      this.x += xstep;
+      this.y += ystep;
+      this.x = min(this.x, width);
+      this.x = max(this.x, 0);
+      this.y = min(this.y, height);
+      this.y = max(this.y, 0);
+    }
   }
 }
 
-let walker;
+const WALKER_COUNT = 100;
+
+let walkers;
 
 function setup() {
 	createCanvas(350, 600);
   background(255);
-	walker = new Walker();
+  walkers = [];
+  for (let i = 0; i < WALKER_COUNT; ++i) {
+  	walkers[i] = new Walker();
+  }
 }
 
 function draw() {
@@ -30,6 +46,8 @@ function draw() {
   // stroke(0, 50);
   // circle(random(width), random(height), 16);
 	// circle(width/2, height/2, 25);
-	walker.show();
-	walker.step();
+  for (let i = 0; i < WALKER_COUNT; ++i) {
+	  walkers[i].show();
+	  walkers[i].step();
+  }
 }
